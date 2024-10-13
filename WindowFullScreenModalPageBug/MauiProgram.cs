dbda22b2ddefcd26b7fa74ed483cfadd5c09f2a3
Takym <15681312+Takym@users.noreply.github.com>
2024-10-14 05:46:38 +0900

    Imitated workaround from the doubly title bars problem

--- WindowFullScreenModalPageBug/MauiProgram.cs
+++ WindowFullScreenModalPageBug/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
 
 namespace WindowFullScreenModalPageBug
 {
@@ -13,12 +15,21 @@ namespace WindowFullScreenModalPageBug
 #if WINDOWS
                 .ConfigureLifecycleEvents(events =>
                 {
-                    // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file 
+                    // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file
                     events.AddWindows(windowsLifecycleBuilder =>
                     {
                         windowsLifecycleBuilder.OnWindowCreated(window =>
                         {
                             window.ExtendsContentIntoTitleBar = false;
+
+#if WORKAROUND
+							// Workaround: (worked on doubly title bars problem)
+							if (window.Content is FrameworkElement elem) {
+								elem.Margin = new(0, -32, 0, 0);
+								Canvas.SetZIndex(elem, 0);
+							}
+#endif // WORKAROUND
+
                             var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
                             var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
                             var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
@@ -32,7 +43,7 @@ namespace WindowFullScreenModalPageBug
                         });
                     });
                 })
-#endif            
+#endif
                 .ConfigureFonts(fonts =>
                 {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+dotnet run --framework net9.0-windows10.0.19041.0 --project WindowFullScreenModalPageBug/WindowFullScreenModalPageBug.csproj
